707c72fd7ae5d56f845274ffee50afea80e66015
shivam kumar jha <shivamjha779@gmail.com>
2021-04-12 19:42:45 +0530

    second

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let userinput = prompt("what would you like to do?");
-const todos = ['homework', 'making breakfast'];
+const todos = ["homework", "making breakfast"];
 
 while (userinput !== "quit") {
-    if(userinput == 'list'){
-        console.log('vvvvv');
-        for(let i = 0; i < todos.length; i++){
-            console.log(`$[i], $[todos[i]]`)
-        }
+  if (userinput == "list") {
+    console.log("vvvvv");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
     }
+  }
+  else if(userinput == "new") {
+      const newtodo = prompt("what is the new todo?")
+  }
 
   userinput = prompt("what would you like to do?");
 }
 console.log("you quited the app!");
-
