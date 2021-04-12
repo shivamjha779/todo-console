da1663b5f60c7e67896592e8e9927f45029a7c77
shivam kumar jha <shivamjha779@gmail.com>
2021-04-12 20:09:02 +0530

    added remove function

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ while (userinput !== "quit") {
     for (let i = 0; i < todos.length; i++) {
       console.log(`${i}: ${todos[i]}`);
     }
-  }
-  else if(userinput == "new") {
-      const newtodo = prompt("what is the new todo?")
+  } else if (userinput == "new") {
+    const newtodo = prompt("what is the new todo?");
+    todos.push(newtodo);
+    console.log(`${newtodo} added to the list`);
+  } else if (userinput == "remove") {
+    const removetodo = prompt("enter the index which you want to remove!");
+    const deleted = todos.splice(removetodo, 1);
+    console.log(`you removed ${deleted[0]}`);
   }
 
   userinput = prompt("what would you like to do?");
