c88eb932b7876789d6e5d86aa056cd08dcc78f60
shivam kumar jha <shivamjha779@gmail.com>
2021-04-13 11:42:47 +0530

    added font and some color styles

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let userinput = prompt("what would you like to do?  | type 'quit' if not seeing instructions on screen");
+let userinput = prompt("what would you like to do? | type 'quit' if not seeing instructions on screen");
 const todos = ["homework", "making breakfast"];
 
 while (userinput !== "quit") {
